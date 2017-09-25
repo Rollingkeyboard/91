1746986f776503f6a411cbf73b85ab283774cb03
jakehu <jakehu1991@gmail.com>
2017-09-25 16:37:44 +0800

    hotfix.增加时间用于区分

--- src2file.py
+++ src2file.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-import redis, math, common
+import redis, math, common, time
 
 # 将每个视频的url写入文件，然后用迅雷拖吧
 c = redis.StrictRedis("localhost", 6379)
@@ -9,7 +9,7 @@ total = len(lst)
 count = math.floor(total / 1000) + 1 # 比如 3005个，需要4个文件，每个文件1000个，最后一个文件5个
 
 for i in range(1, int(count + 1)):
-	s = ""
+	s = "\n\n" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+ "\n\n"
 	for a in lst[(i - 1) * 1000 : i * 1000]:
 		src = a.decode("utf-8")
 		if src != "None":
